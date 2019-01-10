4fa9ce989320a86c4e8701bf501dd9439bfe15b5
jason-sanchez <jsanchez@systemaxcorp.com>
2019-01-10 14:26:52 -0500

    Initial Commit

---
+++ ULHORUMapper/ULHORUMapper/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ULHORUMapper")> 
+<Assembly: AssemblyDescription("20150409 - vs2013 version")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("ULHORUMapper")> 
+<Assembly: AssemblyCopyright("Copyright Systemax Corporation©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0ad9026c-dd3e-4dc5-ac05-ade7daefb173")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("22015.04.09.0")> 
+<Assembly: AssemblyFileVersion("2015.03.02.0")> 
